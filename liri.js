5095cbd6e632f06e28dc46dde91d42d9f0a1477f
Mayra Cuenca <mayra.cuencac@gmail.com>
2019-05-10 20:34:09 -0500

    Added spotify-this-song at its minimum

--- liri.js
+++ liri.js
@@ -4,17 +4,13 @@ require("dotenv").config();
 var axios = require("axios");
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
-//var Omdb = require("omdb");
-//var bandsintown = require('bandsintown')("codingbootcamp");
 var moment = require('moment');
 
 //Access keys
 var spotify = new Spotify(keys.spotify);
-//var omdb = new Omdb(keys.omdb)
 
 var command = process.argv[2];
 var nodeArgs = process.argv;
-
 var search = "";
 for (var i = 3; i < nodeArgs.length; i++) {
     if (i > 3 && i < nodeArgs.length) {
@@ -31,9 +27,15 @@ switch (command) {
         concertThis(search);
         break;
     case ("spotify-this-song"):
-
+        if (search === undefined) {
+            search = "The+Sign";
+        }
+        spotifyThisSong(search);
         break;
     case ("movie-this"):
+       /* if (search === undefined) {
+            search = "Mr.Nobody";
+        }*/
         movieThis(search);
         break;
     case ("do-what-it-says"):
@@ -43,37 +45,36 @@ switch (command) {
         console.log("Please type concert-this / spotify-this-song /movie-this / do-what-it-says + your search. Try again.");
 }//End Switch
 
-
+//For Movie-This
 function movieThis(search) {
-    var queryUrl = "http://www.omdbapi.com/?t=" + search + "&tomatoes=true&apikey=trilogy";
-    console.log(queryUrl);
-    console.log(search);
-    axios.get(queryUrl).then(
-        function (response) {
-            console.log(
-                "\n====================================================" +
-                "\n Title: " + response.data.Title +
-                "\n Release Year: " + response.data.Year +
-                "\n IMDB Rating: " + response.data.imdbRating +
-                "\n Rotten Tomatoes Rating: " + response.data.Ratings[1].Value +
-                "\n Country: " + response.data.Country +
-                "\n Language: " + response.data.Language +
-                "\n Plot: " + response.data.Plot +
-                "\n Actors: " + response.data.Actors +
-                "\n====================================================\n"
+        var queryUrl = "http://www.omdbapi.com/?t=" + search + "&tomatoes=true&apikey=trilogy";
+        axios.get(queryUrl).then(
+            function (response) {
+                console.log(
+                    "\n====================================================" +
+                    "\n Title: " + response.data.Title +
+                    "\n Release Year: " + response.data.Year +
+                    "\n IMDB Rating: " + response.data.imdbRating +
+                    "\n Rotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                    "\n Country: " + response.data.Country +
+                    "\n Language: " + response.data.Language +
+                    "\n Plot: " + response.data.Plot +
+                    "\n Actors: " + response.data.Actors +
+                    "\n====================================================\n"
 
-            );
-        }
-    );
-};
+                );//End console log
+            } //End function
+        );//end axios
+};//end movieThis
 
+//For Concert-This
 function concertThis(search) {
     var bandUrl = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp";
     axios.get(bandUrl).then(
         function (response) {
             if (response.data.length > 0) {
                 console.log("========================================");
-                console.log("Found " + (response.data.length)+ " " + search + " Concerts: ");
+                console.log("Found " + (response.data.length) + " " + search + " Concerts: ");
                 for (var j = 0; j < response.data.length; j++) {
                     console.log(
                         "========================================\n" +
@@ -89,5 +90,29 @@ function concertThis(search) {
             }
         }
     );
+};
+//For Spotify-This-Song
+function spotifyThisSong(search) {
+
+    spotify.search({ type: 'track', query: search })
+        .then(function (response) {
+            console.log(
+                "========================================\n" +
+                "Found " + response.tracks.items.length + " songs"
+            );
+
+            for (var k = 0; k < response.tracks.items.length; k++) {
+                console.log(
+                    "========================================" +
+                    "\n Artist: " + response.tracks.items[k].artists[0].name +
+                    "\n Song: " + response.tracks.items[k].name +
+                    "\n Song Preview URL: " + response.tracks.items[k].preview_url +
+                    "\n Album: " + response.tracks.items[k].album.name
+                );
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 };
 
